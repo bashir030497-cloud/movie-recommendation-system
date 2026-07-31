8f59d372c7737bc1db491d0162ca874829d5642f
Bashir Ahmad <bashir030497@gmail.com>
2026-07-31 13:26:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from huggingface_hub import hf_hub_download
 # ==========================
 
 model_path = hf_hub_download(
-    repo_id="bashirkhan0329/movie_recomendation_system",
+    repo_id="b9a8d6a8678ff0f359f2374102f212672dbd98c93a7602d23d5e15b60707266c",
     filename="knn_model.pkl",
     repo_type="model"
 )
 
 vectors_path = hf_hub_download(
-    repo_id="bashirkhan0329/movie_recommendation_system",
+    repo_id="c980362cbe44f737ccd1efe102f18d60dfcca27b12bcdf4437f033b8258c718c",
     filename="vectors.pkl",
     repo_type="model"
 )
